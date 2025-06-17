b5b392a902c167a163883d8ad851fc9345f218cb
SharveshShiva <152396329+SharveshShiva@users.noreply.github.com>
2025-06-17 22:36:55 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,69 @@
+#You can get a password which is shuffled with letters symbols and numbers..
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = []
+
+for num in range(nr_letters):
+    random_letter = random.choice(letters)
+    print(random_letter)
+    password.append(random_letter)
+
+for num in range(nr_symbols):
+    random_symbol = random.choice(symbols)
+    print(random_symbol)
+    password.append(random_symbol)
+
+for num in range(nr_numbers):
+    random_number = random.choice(numbers)
+    print(random_number)
+    password.append(random_number)
+
+random.shuffle(password)
+pass1 = ("".join(password))
+print(pass1)
+
+# You can get a password without shuffling..
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = ""
+
+for num in range(nr_letters):
+    random_letter = random.choice(letters)
+    password += random_letter
+
+for num in range(nr_symbols):
+    random_symbol = random.choice(symbols)
+    password += random_symbol
+
+for num in range(nr_numbers):
+    random_number = random.choice(numbers)
+    password += random_number
+print(password)
+
+
+
+
+
+
+
+
+
+
